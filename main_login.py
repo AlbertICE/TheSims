33b7914d76fbdb0cedba8aad604f00e64e9f2041
xingbaox <wangxingbao117@163.com>
2016-10-12 22:59:03 +0800

    add charm check

--- main_login.py
+++ main_login.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # _*_coding:utf-8_*_
+#-*-coding:utf-8-*-
 
 #========================================
 __author__ = 'Wang Xingbao'
@@ -23,32 +24,28 @@ import initialize_data as i_n
 import utils
 import random
 
-def random_attri_func(param): #随机人物属性生成字典
-    random_dict = {'height':[160,190,],
-                   'weight':[90,180],
-                   'status':["高兴的","沮丧的","平淡的","发奋的"],
-                   'technology':["C语言","唱歌","跳舞","Java","Python","Shell"],
-                   'job':["None","失业中","网吧网管","快递员","卖报纸"],
-                   'cash':[200,30000],
-                   'salary':[800,2000],
-                   'nationality':["中国","韩国","日本","俄罗斯","美国"],
-                   'charac':["歪瓜裂枣","长相平平","相貌端正","相貌俊俏","人中龙凤"],
-                   'strength':["四肢无力","手无缚鸡","体力正常","身体强壮","力量过人","超人来了"],
-                   'agility':[10,100],
-                   'intelligence':[160,300],
+def random_attri_func(): #随机人物属性生成字典
+    random_dict = {'height':random.choice([[0.5,160],[1,170],[2,180]]),
+                   'weight':random.choice([[0.5,"太轻"],[0.5,"太重"],[2,"正常"],[3,"完美"]]),
+                   'status':random.choice([[2,"高兴的"],[0.5,"沮丧的"],[1,"平淡的"],[3,"发奋的"]]),
+                   'technology':random.choice([[2,"C语言"],[4,"唱歌"],[4,"跳舞"],[3,"Java"],[3,"Python"],[2,"Shell"],[0.5,"None"]]),
+                   'job':random.choice([[0.3,"失业中"],[0.3,"失业中"],[1,"网吧网管"],[2,"快递员"],[0.7,"卖报纸"]]),
+                   'cash':random.randint(10000,100000),
+                   'salary':random.randint(800,8000),
+                   'nationality':random.choice([[1,"中国"],[0.5,"韩国"],[0.5,"日本"],[0.7,"俄罗斯"],[2,"美国"]]),
+                   'charac':random.choice([[0.5,"歪瓜裂枣"],[1,"长相平平"],[3,"相貌端正"],[4,"相貌俊俏"],[5,"人中龙凤"]]),
+                   'strength':random.choice([[0.3,"四肢无力"],[0.5,"手无缚鸡"],[1,"体力正常"],[1.5,"身体强壮"],[2,"力量过人"],[3,"超人来了"],[5,"上帝模式"]]),
+                   'agility':random.randint(10,100),
+                   'intelligence':random.randint(160,300),
+                   'charm':random.randint(2,8)
                    }
-    case: param =
+    print "DEBUGING,random_dict",random_dict
+    return random_dict
 
 
 def random_event_func(param):
-    random_dict = {"":}
+    pass
 
-
-
-def init_data(name,sex):
-    protagonist = utils.Human_Attr
-    protagonist.name = name
-    protagonist.sex = sex
 
 
 def log_in():
@@ -79,7 +76,37 @@ def log_in():
             sex = "男"
         else:
             sex = "女"
-        init_data(name,sex)
+        random_ditc = random_attri_func() #返回人物属性的一个随机字典
+        # 实例化主人公
+        protagonist = utils.Human_Attr
+
+        # 在random_dict中取得主人公的属性
+        protagonist.name = name
+        protagonist.age = 18
+        protagonist.sex = sex
+        protagonist.height = random_ditc['height']
+        protagonist.weight = random_ditc['weight']
+        protagonist.status = random_ditc['status']
+        protagonist.technology = random_ditc['technology']
+        protagonist.job = random_ditc['job']
+        protagonist.cash = random_ditc['cash']
+        protagonist.salary = random_ditc['salary']
+        protagonist.nationality = random_ditc['nationality']
+        protagonist.charac = random_ditc['charac']
+        protagonist.strength = random_ditc['strength']
+        protagonist.agility = random_ditc['agility']
+        protagonist.intelligence = random_ditc['intelligence']
+        base_charm = random_ditc['charm']
+        # 把计算魅力值需要的数据整合成列表传给 魅力值计算方法
+        # reay_calc_charm = [protagonist.age,protagonist.height,protagonist.weight,protagonist.status,protagonist.technology,protagonist.job,protagonist.cash,protagonist.salary,protagonist.strength,protagonist.intelligence]
+        # print "DEBUG......打印传递列表长度", len(reay_calc_charm)
+        protagonist.charm = utils.charm_calc(protagonist) * base_charm
+
+        print '--------------->',protagonist.charm
+        print "_____________________charm should bigger than 200w you win!!!!!!!!!!!!!!!!!!!____________________"
+
+
+
 
 if __name__ == '__main__':
     log_in()
--- utils.py
+++ utils.py
@@ -25,6 +25,8 @@ def random_func():
     random_event = ["中彩票了","的老板高兴了","的老板生气了","遇到了经济危机了","在的城市房价上涨","涨薪水了","什么也没有做","什么也没有做","生病了","什么也没有做","的心情好","的心情坏","去买衣服","什么也没有做","什么也没有做","去旅游了","去看电影了","去图书馆看书了","什么也没有做","与别人打仗了","什么也没有做","什么也没有做"]
     print "在Albert的不明力量的影响下，" + random.choice(random_event) #打印随机事件
     return random_event
+
+
 class Human_Attr(object):
 
 
@@ -35,6 +37,10 @@ class Human_Attr(object):
         self.height = height #身高
         self.weight = weight #体重
         self.status = status #心理状态/高兴/伤心/迷茫/......
+        self.technology =technology #技能
+        self.job = job #工作
+        self.cash = cash #存款
+        self.salary = salary #工资
         self.nationality = nationality #国籍
         self.charac = charac #人物特征
         self.strength = strength #力量
@@ -42,17 +48,13 @@ class Human_Attr(object):
         self.intelligence = intelligence #智力
         self.charm = charm #魅力值
 
-
     def talk(self):
         pass
 
     def walk(self):
         pass
 
-        self.technology =technology #技能
-        self.job = job #工作
-        self.cash = cash #存款
-        self.salary = salary #工资
+
     def study(self):
         pass
 
@@ -69,10 +71,59 @@ class Human_Attr(object):
         pass
 
 
-class Life_Time(object):
+def Life_Time():
 
-    Lift_start = 20
+    Lift_start = 18
     Life_time = 100
+
+
+def charm_calc(people):
+    pass
+    charm_calc_result = ((100 - int(people.age)) * 2 + people.height[0] * 0.8) * people.weight[0]  * people.status[0] * 0.2 * people.technology[0]  * people.job[0] + (people.cash* 0.01 + people.salary * 0.1) * people.charac[0] * 0.8 * people.strength[0] * people.intelligence * 0.6
+                    #((剩余寿命的2倍 +  身高的0.8倍) * 体重指标) * 状态指标的 0.2倍 * 技能指标 * 工作指标 + (存款的百分之一 + 工资的十分之一) * 长相的指标 * 0.8 体力的指标
+
+    # random_dict = {'height':random.choice([[0.5,160],[1,170],[2,180]]),
+    #                'weight':random.choice([[0.5,"太轻"],[0.5,"太重"],[2,"正常"],[3,"完美"]]),
+    #                'status':random.choice([[2,"高兴的"],[0.5,"沮丧的"],[1,"平淡的"],[3,"发奋的"]]),
+    #                'technology':random.choice([[2,"C语言"],[4,"唱歌"],[4,"跳舞"],[3,"Java"],[3,"Python"],[2,"Shell"],[0.5,"None"]]),
+    #                'job':random.choice([[0.3,"失业中"],[0.3,"失业中"],[1,"网吧网管"],[2,"快递员"],[0.7,"卖报纸"]]),
+    #                'cash':random.randint(10000,100000),
+    #                'salary':random.randint(800,8000),
+    #                'nationality':random.choice([[1,"中国"],[0.5,"韩国"],[0.5,"日本"],[0.7,"俄罗斯"],[2,"美国"]]),
+    #                'charac':random.choice([[0.5,"歪瓜裂枣"],[1,"长相平平"],[3,"相貌端正"],[4,"相貌俊俏"],[5,"人中龙凤"]]),
+    #                'strength':random.choice([[0.3,"四肢无力"],[0.5,"手无缚鸡"],[1,"体力正常"],[1.5,"身体强壮"],[2,"力量过人"],[3,"超人来了"],[5,"上帝模式"]]),
+    #                'agility':random.randint(10,100),
+    #                'intelligence':random.randint(160,300),
+    #                'charm':random.randint(50,100)
+    #                }
+
+    print "_____DEBUG______charm_calc_result is ------------------------------->>>>>>>> ready to add base:",charm_calc_result
+
+
+    #根据传过来的人物属性参数得到人物魅力值并传回
+    return charm_calc_result
+
+def feel_check(people):
+    pass
+    # calc
+    return "good !"
+
+
+
+
+def print_out_message(people):
+    pass
+    people.height
+
+    print '''现在让我们来看看你如今的成就吧！
+    xxx到目前已经存活了xxx年。
+    经过你的不懈的努力和莫名的命运之神的干预，如今你是xxx（职业）。
+    身为xxx（国人），到也长得xxxx，体力上算是xxxx了。
+    工资也不算太少，辛辛苦苦攒了xxxx。
+    '''
+    print "目前女主人公xxx对你", feel_check(people)
+
+
 
 
 if __name__ == '__main__':
